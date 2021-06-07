657b3e4e641d9b22a07cf961793b11d1327c2a15
Suzan Hiary <suze-hiary@hotmail.com>
2021-06-07 16:27:07 +0300

    added some pic

---
+++ JS/app.js
@@ -0,0 +1,62 @@
+let var1 = prompt('please enter your name ?');
+console.log(var1);
+alert('Hello ' + var1);
+
+
+let var2 = prompt ('Did you achieve your dream??').toLowerCase();
+console.log(var2);
+if (var2==='yes'|| var2=== 'y') {
+    alert('Congrats that is awesome ^_^') ;
+}
+if (var2==='no'|| var2==="n"){
+    alert(' i hope you will do soon ^_^');
+}
+else {  console.log(var2)
+};
+
+let var3 = prompt ('are you still styding??').toLowerCase();
+console.log(var3);
+if (var3==='yes'|| var3=== 'y') {
+    alert(' you have a long trip ') ;
+}
+if (var3==='no'|| var3==="n"){
+    alert(' that is good you are adult one');
+} else {
+    console.log(var3);
+}
+
+
+
+let var4 = prompt ('do you have brothers??').toLowerCase();
+console.log(var4);
+if (var4==='yes'|| var4=== 'y') {
+    alert(' family is the most beautiful thing ^_^ ') ;
+}
+if (var4==='no'|| var4==="n"){
+    alert(' that is ok');
+} else {
+    console.log(var4);
+}
+
+let var5 = prompt ('are you funny person??').toLowerCase();
+console.log(var5);
+if (var5==='yes'|| var5=== 'y') {
+    alert(' congrats ^_^ ' + var1 ) ;
+}
+if (var5==='no'|| var5==="n"){
+    alert( var1 + ' you must be!!');
+} else {
+    console.log(var5);
+}
+
+
+let var6 = prompt ('did you enjoy in my page??').toLowerCase();
+console.log(var6);
+if (var6==='yes'|| var6=== 'y') {
+    alert(' thanks^_^ ' + var1) ;
+}
+if (var6==='no'|| var6==="n"){
+    alert(var1 + ' i hope ypu will change your mind the next time!!');
+} else {
+    console.log(var6);
+}
