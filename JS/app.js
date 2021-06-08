5b29c44de5f93a4c00e9c9c0884f1081f0bd9dba
SuzanHiary <>
2021-06-08 16:04:50 +0000

    added the 6th and the 7th Q

--- JS/app.js
+++ JS/app.js
@@ -1,62 +1,116 @@
+let d = 0;
 let var1 = prompt('please enter your name ?');
 console.log(var1);
 alert('Hello ' + var1);
 
 
-let var2 = prompt ('Did you achieve your dream??').toLowerCase();
+let var2 = prompt ('Did i achieve my dream??').toLowerCase();
 console.log(var2);
 if (var2==='yes'|| var2=== 'y') {
-    alert('Congrats that is awesome ^_^') ;
+    alert('not correct ') ;
 }
 if (var2==='no'|| var2==="n"){
-    alert(' i hope you will  soon ^_^');
+    alert(' correct');
+    d++;
 }
 else {  console.log(var2)
 };
 
-let var3 = prompt ('are you still styding??').toLowerCase();
+let var3 = prompt ('do i still styding??').toLowerCase();
 console.log(var3);
 if (var3==='yes'|| var3=== 'y') {
-    alert(' you have a long trip ') ;
+    alert(' not correct ') ;
 }
 if (var3==='no'|| var3==="n"){
-    alert(' that is good you are adult one');
+    alert(' correct');
+    d++;
 } else {
     console.log(var3);
 }
 
 
 
-let var4 = prompt ('do you have brothers??').toLowerCase();
+let var4 = prompt ('do i have brothers??').toLowerCase();
 console.log(var4);
 if (var4==='yes'|| var4=== 'y') {
-    alert(' family is the most beautiful thing ^_^ ') ;
+    alert(' correct i have two ') ;
+    d++;
 }
 if (var4==='no'|| var4==="n"){
-    alert(' that is ok');
+    alert('not correct' );
 } else {
     console.log(var4);
 }
 
-let var5 = prompt ('are you funny person??').toLowerCase();
+let var5 = prompt ('am i funny person??').toLowerCase();
 console.log(var5);
 if (var5==='yes'|| var5=== 'y') {
-    alert(' congrats ^_^ ' + var1 ) ;
+    alert(' correct ' + var1 ) ;
+    d++;
 }
 if (var5==='no'|| var5==="n"){
-    alert( var1 + ' you must be!!');
+    alert( var1 + '!!  not correct ');
 } else {
     console.log(var5);
 }
 
 
-let var6 = prompt ('did you enjoy in my page??').toLowerCase();
+let var6 = prompt ('am i winter lover??').toLowerCase();
 console.log(var6);
 if (var6==='yes'|| var6=== 'y') {
-    alert(' thanks^_^ ' + var1) ;
+    alert(' not correct^_^ ' + var1) ;
 }
 if (var6==='no'|| var6==="n"){
-    alert(var1 + ' i hope you will change your mind the next time!!');
+    alert(var1 + ' correct!');
+    d++;
 } else {
     console.log(var6);
 }
+
+let var7 = prompt('can you guess the number i liked ?');
+
+let k;
+let number_of_tries = 4;
+for (k = 1; k < 4; k++) {
+    if (var7== 3) {
+        console.log(var7);
+        alert('Correct Answer');
+        d++;
+        break;
+    } else if (var7 > 3) {
+        var7 = prompt('too high please try again ,Number of tries Left: ' + (number_of_tries - k));
+
+    } else
+    if (var7 < 3) {
+        var7 = prompt('too low please try again ,Number of tries Left: ' + (number_of_tries - k));
+    }
+    if (k == 3) {
+        alert('sorry you have reached the limit of tries - correct answer is 3');
+    }
+
+}
+
+
+
+
+let a;
+let var8= prompt('What is the color of Apple??');
+let color = ['red', 'yellow', 'green'];
+let number_of_tries_postive = 7;
+for (a = 1; a < 7; a++) {
+
+    if (var8 === 'red' || var8 === 'yellow' || var8 === "green") {
+        console.log('correct');
+        alert('correct!');
+        d++;
+        break;
+    } else {
+        var8 = prompt('that is wrong try again! ,Number of tries Left :' + (number_of_tries_postive - a));
+    }
+    if (a == 6) {
+        alert('sorry  The correct answer is red or yellow or green');
+
+    }
+}
+
+alert('your score is : ' + d)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # About-Me
 Hello, My name is Suzan Hiary I'm 25 years old from salt jordan i'm a civil engineering this is my first project in programming.
+
+I create a fun gussing game five yes/no question and the 6th gusse the number i liked and the 7th to answer the question with multiplication answers, i hope you enjoying in my web page.
